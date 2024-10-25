f38ec2031f59aca2e029719dec1a7c5dc23f4e66
saisruthiganugu <ssg3043sruthi@gmail.com>
2024-10-25 12:57:21 +0530

    Update DaemonThread.java

--- DaemonThread/DaemonThread.java
+++ DaemonThread/DaemonThread.java
@@ -1,21 +1,23 @@
-public class TestDaemonThread1 extends Thread{  
- public void run(){  
-  if(Thread.currentThread().isDaemon()){//checking for daemon thread  
-   System.out.println("daemon thread work");  
-  }  
-  else{  
-  System.out.println("user thread work");  
- }  
- }  
- public static void main(String[] args){  
-  TestDaemonThread1 t1=new TestDaemonThread1();//creating thread  
-  TestDaemonThread1 t2=new TestDaemonThread1();  
-  TestDaemonThread1 t3=new TestDaemonThread1();  
-  
-  t1.setDaemon(true);//now t1 is daemon thread  
-    
-  t1.start();//starting threads  
-  t2.start();  
-  t3.start();  
- }  
-}  
+package com.example.springOne.DaemonThread;
+
+public class DaemonThread extends Thread {
+    public void run() {
+        if (Thread.currentThread().isDaemon()) {//checking for daemon thread
+            System.out.println("daemon thread work");
+        } else {
+            System.out.println("user thread work");
+        }
+    }
+
+    public static void main(String[] args) {
+        DaemonThread t1 = new DaemonThread();//creating thread
+        DaemonThread t2 = new DaemonThread();
+        DaemonThread t3 = new DaemonThread();
+
+        t1.setDaemon(true);//now t1 is daemon thread
+
+        t1.start();//starting threads
+        t2.start();
+        t3.start();
+    }
+}
